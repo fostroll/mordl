f8ebbeed0836438402933101b7bb20f19d3a27b7
fostroll <fostroll@gmail.com>
2020-07-03 02:27:19 +0300

    editing

--- mordl/lstm_tagger_model.py
+++ mordl/lstm_tagger_model.py
@@ -5,6 +5,7 @@
 # License: BSD, see LICENSE for details
 """
 """
+from collections.abc import Iterable
 from junky import CharEmbeddingRNN, CharEmbeddingCNN, Masking
 from mordl.base_model import BaseModel
 import torch
@@ -17,10 +18,12 @@ class LstmTaggerModel(BaseModel):
 
     def __init__(self, tags_count, tags_pad_idx=None, vec_emb_dim=None,
                  alphabet_size=0, char_pad_idx=0, rnn_emb_dim=None,
-                 cnn_emb_dim=None, cnn_kernels=list(range(1, 7)),
+                 cnn_emb_dim=None, cnn_kernels=range(1, 7),
                  emb_out_dim=512, lstm_hidden_dim=256, lstm_layers=1,
                  lstm_do=0, bn1=True, do1=.2, bn2=True, do2=.5,
                  bn3=True, do3=.4):
+        if isinstance(cnn_kernels, Iterable):
+            cnn_kernels = list(cnn_kernels)
         super().__init__(
             tags_count, tags_pad_idx=tags_pad_idx, vec_emb_dim=vec_emb_dim,
             alphabet_size=alphabet_size, char_pad_idx=char_pad_idx,
