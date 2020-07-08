c564a13cb163c348c5bfdc92b86b6f1cc7b735ec
fostroll <fostroll@gmail.com>
2020-07-08 16:20:00 +0300

    ed

--- mordl/upos_tagger.py
+++ mordl/upos_tagger.py
@@ -120,7 +120,7 @@ class UposTagger(BaseTagger):
             print('Evaluate ' + header, file=LOG_FILE)
         n = c = 0
         i = -1
-        for i, (gold_sent, test_sent) in enumerate(zip(gold, test)):
+        for i, (gold_sent, test_sent) in enumerate(test):
             for gold_token, test_token in zip(
                 gold_sent[0] if isinstance(gold_sent, tuple) else gold_sent,
                 test_sent[0] if isinstance(test_sent, tuple) else test_sent
