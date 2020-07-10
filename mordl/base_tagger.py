ba1516b1223ab288e77378f39eabafa153630c6c
fostroll <fostroll@gmail.com>
2020-07-10 23:30:31 +0300

    ed

--- mordl/base_tagger.py
+++ mordl/base_tagger.py
@@ -147,13 +147,14 @@ class BaseTagger(BaseParser):
                 if not res_:
                     res_ = ds_.transform_collate(sentences,
                                                  batch_size=batch_size)
-                res.append(res_)
+                res += res_ if isinstance(res_, tuple) else [res_]
             elif typ == 't':
-                res.append(ds_.transform_collate(tags[int(idx)],
-                                                 batch_size=batch_size))
+                res_ = ds_.transform_collate(tags[int(idx)],
+                                             batch_size=batch_size)
+                res += res_ if isinstance(res_, tuple) else [res_]
             elif labels and typ == 'y':
-                res.append(ds_.transform_collate(labels,
-                                                 batch_size=batch_size))
+                res_ = ds_.transform_collate(labels, batch_size=batch_size)
+                res += res_ if isinstance(res_, tuple) else [res_]
         return zip(*res)
 
     def _save_dataset(self, model_name):
