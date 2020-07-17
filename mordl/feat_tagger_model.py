a0d91e5936f744176cf3d1fd3c8c9e4a5172c77b
Anastasia Nikiforova <steysie@gmail.com>
2020-07-17 19:00:23 +0300

    added help to feat, ne, upos tagger models

--- mordl/feat_tagger_model.py
+++ mordl/feat_tagger_model.py
@@ -12,7 +12,76 @@ from mordl.defaults import CONFIG_ATTR
 
 
 class FeatTaggerModel(BaseTaggerModel):
+    """Named entity tagger class.
 
+    Args:
+
+    **labels_num** (`int`): number of target labels.
+
+    **labels_pad_idx** (`int`): index of padding element in the label
+    vocabulary.
+
+    **vec_emb_dim** (`int`): word-level embedding vector space dimensionality.
+     If `None`, the layer is skipped.
+
+    **alphabet_size** (`int`): length of character vocabulary. Relevant with
+    not `None` **rnn_emb_dim** or **cnn_emb_dim**.
+
+    **char_pad_idx** (`int`): index of padding element in the character
+    vocabulary. Relevant with not `None` **rnn_emb_dim** or **cnn_emb_dim**.
+
+    **rnn_emb_dim** (`int`): character RNN (LSTM) embedding dimensionality. If
+    `None`, the layer is skipped.
+
+    **cnn_emb_dim** (`int`): character CNN embedding dimensionality. If
+    `None`, the layer is skipped.
+
+    **cnn_kernels** (`list([int])`): CNN kernel sizes. By default,
+    `cnn_kernels=[1, 2, 3, 4, 5, 6]`. Relevant with not `None`
+    **cnn_emb_dim**.
+
+    **tag_emb_params** (`dict({'dim': int, 'num': int, 'pad_idx': int})` |
+    `list([dict])`): params of the embedding layers for additional
+    `junky.dataset.TokenDataset` outputs. If `None`, the layers are not
+    created.
+
+    **upos_emb_dim** (`int`): auxiliary UPOS label embedding dimensionality.
+    Default `upos_emb_dim=60`.
+
+    **upos_num** (`int`): length of UPOS vocabulary.
+
+    **upos_pad_idx** (`int`): index of padding element in the UPOS vocabulary.
+
+    **emb_out_dim** (`int`): output embedding dimensionality. Default
+    `emb_out_dim=512`.
+
+    **lstm_hidden_dim** (`int`): Bidirectional LSTM hidden size. Default
+    `lstm_hidden_dim=256`.
+
+    **lstm_layers** (`int`): number of Bidirectional LSTM layers. Default
+    `lstm_layers=1`.
+
+    **lstm_do** (`float`): dropout between LSTM layers. Only relevant, if
+    `lstm_layers` > `1`.
+
+    **bn1** (`bool`): whether batch normalization layer should be applied
+    after the embedding layer. Default `bn1=True`.
+
+    **do1** (`float`): dropout rate after the first batch normalization
+    layer `bn1`. Default `do1=.2`.
+
+    **bn2** (`bool`): whether batch normalization layer should be applied
+    after the linear layer before LSTM layer. Default `bn2=True`.
+
+    **do2** (`float`): dropout rate after the second batch normalization
+    layer `bn2`. Default `do2=.5`.
+
+    **bn3** (`bool`): whether batch normalization layer should be applied
+    after the LSTM layer. Default `bn3=True`.
+
+    **do3** (`float`): dropout rate after the third batch normalization
+    layer `bn3`. Default `do3=.4`.
+    """
     def __init__(self, labels_num, labels_pad_idx=None, vec_emb_dim=None,
                  alphabet_size=0, char_pad_idx=0, rnn_emb_dim=None,
                  cnn_emb_dim=None, cnn_kernels=[1, 2, 3, 4, 5, 6],
