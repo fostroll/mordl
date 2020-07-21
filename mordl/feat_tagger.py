1291dce0cbff050051990b7febfaa478af5c7505
fostroll <fostroll@gmail.com>
2020-07-21 17:39:55 +0300

    ed

--- mordl/feat_tagger.py
+++ mordl/feat_tagger.py
@@ -19,15 +19,19 @@ class FeatTagger(BaseTagger):
 
     `feats_clip_coef=0` means "not use feats"
     `feats_clip_coef=None` means "use all feats"
-    relevant only for feat not from FEATS
+    relevant only if cdict is specified and field is not from FEATS
     """
-    def __init__(self, field, feats_clip_coef=6):
+    def __init__(self, field, cdict=None, feats_clip_coef=0):
         super().__init__()
         if field.find(':') == -1:
             field = 'FEATS:' + field
         self._field = field
-        if field.startswith('FEATS:'):
-            feats_clip_coef = 0
+        assert not (cdict and field.startswith('FEATS:')),
+            'ERROR: cdict must be None for FEATS fields'
+        assert cdict or feats_clip_coef == 0,
+            'ERROR: feats_clip_coef must be zero if cdict is not specified'
+        if cdict:
+            self._load_cdict(cdict, log_file=log_file)
         self._feats_clip_coef = feats_clip_coef
 
     def _transform_upos(self, corpus, key_vals=None):
@@ -101,8 +105,6 @@ class FeatTagger(BaseTagger):
         """
         args, kwargs = get_func_params(FeatTagger.load, locals())
         super().load(FeatTaggerModel, *args, **kwargs)
-        if feats_clip_coef != 0:
-            self._load_cdict(name + '.cdict.pickle', log_file=log_file)
 
     def predict(self, corpus, with_orig=False, batch_size=BATCH_SIZE,
                 split=None, clone_ds=False, save_to=None, log_file=LOG_FILE):
@@ -162,7 +164,6 @@ class FeatTagger(BaseTagger):
                 corpus = self._get_corpus(save_to, asis=True, log_file=log_file)
 
         return corpus
-
 
     def evaluate(self, gold, test=None, label=None, batch_size=BATCH_SIZE,
                  split=None, clone_ds=False, log_file=LOG_FILE):
@@ -331,11 +332,6 @@ class FeatTagger(BaseTagger):
         Returns the train statistics.
         """
         if self._feats_clip_coef != 0:
-            self._cdict = CorpusDict(corpus=self._train_corpus,
-                                     format='conllu_parsed',
-                                     log_file=log_file)
-            self._save_cdict(save_as + '.cdict.pickle')
-
             list(self._transform_upos(self._train_corpus))
             key_vals = set(x['UPOS'] for x in self._train_corpus for x in x
                                if x['FORM'] and x['UPOS']
