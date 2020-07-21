6417a72b968e1510247c48bf366065cb5ce91b2f
fostroll <fostroll@gmail.com>
2020-07-21 20:38:44 +0300

    ed

--- mordl/ne_tagger.py
+++ mordl/ne_tagger.py
@@ -14,7 +14,5 @@ class NeTagger(FeatTagger):
     """
     Named entity tagger class.
     """
-    def __init__(self, cdict=None, feats_clip_coef=6, log_file=LOG_FILE):
-        super().__init__('MISC:NE', cdict=cdict,
-                         feats_clip_coef=feats_clip_coef if cdict else 0,
-                         log_file=log_file)
+    def __init__(self, feats_clip_coef=6):
+        super().__init__('MISC:NE', feats_clip_coef=feats_clip_coef)
