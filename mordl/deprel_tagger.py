ac480fabe071458a308f2002f95d5459a70e7909
fostroll <fostroll@gmail.com>
2020-08-01 19:50:42 +0300

    ed

--- mordl/deprel_tagger.py
+++ mordl/deprel_tagger.py
@@ -66,7 +66,9 @@ class DeprelTagger(FeatTagger):
                 s.append(token)
                 for window_right_ in window_right(sent, link_id, ids,
                                                   chains, WINDOW_RIGHT):
-                    yield s[:] + window_right_, idx, label
+                    s_ = [[PAD_TOKEN] * (WINDOW_LEFT - 1 - len(s))] \
+                       + s[:] + window_right_
+                    yield s_, idx, label
                 for data_ in next_sent(sent, s, link_id, ids, chains):
                     yield data_
 
