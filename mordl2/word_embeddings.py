e86df5c5d8ba58bac512a8b250456622edf87f79
fostroll <fostroll@gmail.com>
2021-09-29 19:04:24 +0300

    .

--- mordl2/word_embeddings.py
+++ mordl2/word_embeddings.py
@@ -282,11 +282,11 @@ class WordEmbeddings:
 
         config = getattr(train_ds_bert[0], CONFIG_ATTR, None)
         if res and res['best_epoch'] is not None:
-            config.emb_path = save_as
-        emb_config = AutoConfig.from_pretrained(config.emb_path,
+            config['emb_path'] = save_as
+        emb_config = AutoConfig.from_pretrained(config['emb_path'],
                                                 output_hidden_states=True,
                                                 output_attentions=False)
-        emb_model = AutoModel.from_pretrained(config.emb_path,
+        emb_model = AutoModel.from_pretrained(config['emb_path'],
                                               config=emb_config)
         train_ds_bert[0].model = test_ds_bert[0].model = emb_model
         train_ds_bert[0].transform(train_sents, **transform_kwargs)
